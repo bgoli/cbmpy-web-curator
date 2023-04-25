fcf4b0245ab6ce7af2ae635ea9d9d92f05f4c3a7
bgoli <brettolivier@gmail.com>
2023-04-25 10:44:43 +0200

    test runner tweak

--- offline-curator/run_frogg_tests.bat
+++ offline-curator/run_frogg_tests.bat
@@ -2,8 +2,8 @@
 
 echo RUNNNING FROGG TEST MODELS SMALLEST TO LARGEST
 
-python offline_curator.py ..\test-models\frogg\VvuMBEL943_2022.xml
-::python offline_curator.py ..\test-models\frogg\iDS372.xml
+::python offline_curator.py ..\test-models\frogg\VvuMBEL943_2022.xml
+python offline_curator.py ..\test-models\frogg\iDS372.xml
 ::python offline_curator.py ..\test-models\frogg\Gmetallireducens_ac_Fe+Updated.xml
 ::python offline_curator.py ..\test-models\frogg\Canto-Encalada2022-FBA+of+simultaneous+degradation+of+ammonia+and+pollutants.xml
 ::python offline_curator.py ..\test-models\frogg\iJN1463.xml
